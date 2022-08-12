09eb5cf659424fa3e3d650befa41c082b3e025d5
kykurniawan <contact.rizkykurniawan@gmail.com>
2022-08-13 00:58:10 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+console.log('Who is kykurniawan?')
+console.table({
+    name: 'Rizky Kurniawan',
+    email: 'mail@rizkykurniawan.id',
+    web: 'https://www.rizkykurniawan.id',
+    instagram: 'https://www.instagram.com/qwertykurniawan',
+    linkedin: 'https://www.linkedin.com/in/kykurniawan',
+    youtube: 'https://www.youtube.com/kykurniawan',
+    github: 'https://github.com/kykurniawan',
+    npmjs: 'https://www.npmjs.com/~kykurniawan',
+    gitlab: 'https://gitlab.com/kykurniawan',
+    medium: 'https://kykurniawan.medium.com',
+    codepen: 'https://codepen.io/kykurniawan',
+    gitbook: 'https://kykurniawan.gitbook.io/kykurniawan',
+    packagist: 'https://packagist.org/users/kykurniawan',
+    dockerhub: 'https://hub.docker.com/u/kykurniawan',
+})
